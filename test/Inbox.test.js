7a53c097cd9e51ea54e64b8fb1af1e51efdc57bb
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-04-18 14:50:58 +0530

    Testing contract with mocha using async/await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,15 +3,15 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+let accounts;
 
-beforeEach(() => {
+beforeEach(async () => {
   //get a list of all accounts
-  web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-      console.log(fetchedAccounts);
-    });
+  accounts = await web3.eth.getAccounts();
 });
 
 describe('Inbox', () => {
-  it('deploys a contract', () => {})
+  it('deploys a contract', () => {
+    console.log(accounts);
+  })
 });
